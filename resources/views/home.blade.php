5bc7f728eb30e3b4c7c812718df3f0fc242ed5b0
Fenslau <c01@bk.ru>
2021-08-13 21:39:12 +0700

    Тест коммита

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,11 @@
 @section('content')
   <h1>Главная страница</h1>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates, blanditiis aspernatur laboriosam culpa rerum neque tempore nobis numquam ipsa earum. Laudantium, consectetur nesciunt. Sunt, adipisci doloremque quis culpa repellendus quam.</p>
+
+
+  <p>Это свежие изменения в файле </p>
+
 @endsection
-  
 
 @section('aside')
   @parent
