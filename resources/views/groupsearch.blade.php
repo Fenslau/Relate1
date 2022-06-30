44d8d5033e9652ba8f36609b4e43da7692a2d3b9
Fenslau <c01@bk.ru>
2022-06-30 15:28:40 +0700

    Разметка страницы группы ВК

--- resources/views/groupsearch.blade.php
+++ resources/views/groupsearch.blade.php
@@ -9,14 +9,14 @@
   @include('inc.toast')
   <form class="" id="search-submit" action="{{ route('groupsearch') }}" method="post">
     @csrf
-    <h2 class="m-3 text-center text-uppercase" >Поиск групп вконтакте</h2>
+    <h2 itemprop="headline" class="m-3 text-center text-uppercase" >Поиск групп вконтакте</h2>
     @include('inc.buttons-and-progress', ['link' => 'groupsearch', 'button' => 'Найти группы'])
 
-    <div class="search-form row pb-5">
+    <div itemscope itemtype="http://schema.org/Article" class="search-form row pb-5">
       <div class="col-md-8 ">
 
         <div class="row">
-          <div class="lh-m col-md-8 bg-secondary text-white text-right p-3">
+          <div itemprop="articleBody" class="lh-m col-md-8 bg-secondary text-white text-right p-3">
             <label for="name"><span class="h5 d-block font-weight-bold text-uppercase">Название группы</span>
             Введите название группы или его часть в виде ключевого слова для поиска, Если вы хотите найти группы, указавшие город в своем назваии, то вводите название города в этой строке</label>
           </div>
@@ -27,7 +27,7 @@
         </div>
 
         <div class="row">
-          <div class="lh-m col-md-8 bg-secondary text-white text-right p-3">
+          <div itemprop="articleBody" class="lh-m col-md-8 bg-secondary text-white text-right p-3">
             <label for="city"><span class="h5 d-block font-weight-bold text-uppercase">Город</span>
             Название города нужно вводить, если вы ищете группы, ЗАРЕГИСТРИРОВАННЫЕ в каком-то конкретном городе. Если город - это часть названия группы, при необходимости введите его в строку "название группы", а это поле оставьте пустым</label>
           </div>
@@ -49,7 +49,7 @@
         </div>
 
         <div class="row">
-          <div class="lh-m col-md-8 bg-secondary text-white text-right p-3">
+          <div itemprop="articleBody" class="lh-m col-md-8 bg-secondary text-white text-right p-3">
             <label for="wall"><span class="h5 d-block font-weight-bold text-uppercase">Группы с открытой стеной</span>
             Поиск групп, на стене которых любой пользователь может создавать посты и осталять комментарии</label>
           </div>
@@ -59,7 +59,7 @@
         </div>
 
         <div class="row">
-          <div class="lh-m col-md-8 bg-secondary text-white text-right p-3">
+          <div itemprop="articleBody" class="lh-m col-md-8 bg-secondary text-white text-right p-3">
             <label for="comments"><span class="h5 d-block font-weight-bold text-uppercase">Группы с открытыми комментариями к постам</span>
             Поиск групп, на стенах которых можно осталять только комментарии</label>
           </div>
@@ -69,7 +69,7 @@
         </div>
 
         <div class="row">
-          <div class="lh-m col-md-8 bg-secondary text-white text-right p-3">
+          <div itemprop="articleBody" class="lh-m col-md-8 bg-secondary text-white text-right p-3">
             <label for="verify"><span class="h5 d-block font-weight-bold text-uppercase">Верифицированное сообщество</span>
             Это официальные группы известых личностей, музыкантов и компаний, подтвердившие свой статус и помеченные специальным символом "V"</label>
           </div>
@@ -79,7 +79,7 @@
         </div>
 
         <div class="row">
-          <div class="lh-m col-md-8 bg-secondary text-white text-right p-3">
+          <div itemprop="articleBody" class="lh-m col-md-8 bg-secondary text-white text-right p-3">
             <label for="market"><span class="h5 d-block font-weight-bold text-uppercase">Группы с магазином</span>
             Поиск групп, на стене которых есть раздел с продажей товаров или услуг</label>
           </div>
@@ -89,7 +89,7 @@
         </div>
 
         <div class="row">
-          <div class="lh-m col-md-8 bg-secondary text-white text-right p-3">
+          <div itemprop="articleBody" class="lh-m col-md-8 bg-secondary text-white text-right p-3">
             <label for="open"><span class="h5 d-block font-weight-bold text-uppercase">Открытое сообщество</span>
             Группы, в которые можно вступать всем	</label>
           </div>
@@ -105,7 +105,7 @@
         <div class="font-weight-bold text-pink h1 float-left px-3 pt-0 m-0">
           1
         </div>
-        <div class="font-weight-light mb-3">
+        <div itemprop="articleBody" class="font-weight-light mb-3">
           <span class="h6 d-block font-weight-bold">Сформулируйте запрос</span>
           Определите ключевое слово и введите его в поле «Название группы», если в названиях групп присутствует название города, то укажите его здесь. Если вам требуется найти группы ВК, указавшие в настройках профиля название города, то введите его в поле «Город», во всех других случаях оставьте это поле пустым.
         </div>
@@ -113,7 +113,7 @@
         <div class="font-weight-bold text-pink h1 float-left px-3 pt-0 m-0">
           2
         </div>
-        <div class="font-weight-light mb-3">
+        <div itemprop="articleBody" class="font-weight-light mb-3">
           <span class="h6 d-block font-weight-bold">Задайте критерии для поиска групп</span>
           Укажите количество подписчиков от и/или до, поставьте галочки напротив важных для вас характеристик поиска. Если вы хотите собрать все группы по ключевому слову или по принадлежности к городу, то другие поля оставьте пустыми.
         </div>
@@ -121,7 +121,7 @@
         <div class="font-weight-bold text-pink h1 float-left px-3 pt-0 m-0">
           3
         </div>
-        <div class="font-weight-light mb-3">
+        <div itemprop="articleBody" class="font-weight-light mb-3">
           <span class="h6 d-block font-weight-bold">Поиск групп</span>
           Нажмите кнопку «Поиск групп». После того как система соберет все группы, в верхней части экрана появится ссылка на скачивание Excel-таблицы, а внизу экрана отобразится первая 1000 групп из файла. ВАЖНО! В бесплатной версии система показывает на экране и в файле только 10 групп из общего количества найденных. Чтобы скачать все группы, оплатите доступ на 3 дня, 1 месяц или 3 месяца. Информацию о тарифах вы можете найти <a href="{{ route('tarifs') }}">здесь</a> или в самом верху экрана.
         </div>
